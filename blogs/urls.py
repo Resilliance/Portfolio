9aed49df5d6938275de6380a2c15743be9345d19
Resilliance <Jkesguerra2050@yahoo.com>
2022-04-22 10:56:27 -0400

    Logo and navbar routing

--- blogs/urls.py
+++ blogs/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about.html', views.about, name='about'),
-    path('posts.html', views.posts, name='posts'),
-    path('projects.html', views.projects, name='projects'),
-    path('base.html', views.base, name='base'),
+    path('about/', views.about, name='about'),
+    path('posts/', views.posts, name='posts'),
+    path('projects/', views.projects, name='projects'),
+    path('base/', views.base, name='base'),
 
 ]
