1da31cd4d927a118dc6745ff15c13da01034ed99
Resilliance <Jkesguerra2050@yahoo.com>
2022-04-29 12:46:18 -0400

    updated debugging settings

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jdk@08!e&$9c8i%dl*ib&m*hr_b*grk6ftzj*z#)73*@=xd=cg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '0.0.0.0',
