67e0e542b2230e5dea63f5937f2f2d3e1023a6e3
Resilliance <Jkesguerra2050@yahoo.com>
2022-04-29 11:27:17 -0400

    updated root

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -126,11 +126,11 @@ USE_TZ = True
 STATIC_URL = 'staticfiles/'
 MEDIA_URL = '/images/'
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
-STATICFILES_DIRS = [
+"""STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles')
-]
+]"""
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
